a3f12a59118a0fd688c6773440a5ffda76fc5d51
awdsza <37355034+awdsza@users.noreply.github.com>
2022-07-15 00:50:14 +0900

    feat회원가입 Modal 컴포넌트 기본 준비

---
+++ src/views/login/SignupFormView.vue
@@ -0,0 +1,18 @@
+<template><SignupForm v-show="open"></SignupForm></template>
+
+<script>
+import SignupForm from '@/components/login/SignupForm.vue';
+export default {
+  components: {
+    SignupForm,
+  },
+  props: {
+    open: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style></style>
