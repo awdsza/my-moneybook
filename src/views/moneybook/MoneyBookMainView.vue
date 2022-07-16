67c707d23cc14568103f98b273f65a9d4706f1e6
awdsza <37355034+awdsza@users.noreply.github.com>
2022-07-17 02:00:50 +0900

    feat:메인페이지 최상단 header

--- src/views/moneybook/MoneyBookMainView.vue
+++ src/views/moneybook/MoneyBookMainView.vue
@@ -1,14 +1,17 @@
 <template>
   <section>
+    <AppHeader />
     <MoneyBookMain />
   </section>
 </template>
 
 <script>
 import MoneyBookMain from '@/components/moneybook/MoneyBookMain.vue';
+import AppHeader from '../../components/common/AppHeader.vue';
 export default {
   components: {
     MoneyBookMain,
+    AppHeader,
   },
 };
 </script>
