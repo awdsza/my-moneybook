816dfff30b3c8df947e5768af0dd2686834a6342
awdsza <37355034+awdsza@users.noreply.github.com>
2022-07-31 17:15:24 +0900

    feat:회원가입API호출 추가.

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
-
+import { post } from '@/api/index';
 export default new Vuex.Store({
   state: {
     isLogin: false,
   },
   mutations: {},
   getters: {},
-  actions: {},
+  actions: {
+    async signUpUser(context, payload) {
+      return await post('users', payload);
+    },
+  },
 });
