5ecd383ddedfdc2d5d7f4b57e08f22637f3dfe87
awdsza <37355034+awdsza@users.noreply.github.com>
2022-08-16 00:23:37 +0900

    feat:날짜 클릭 이벤트 구현.

--- src/components/accountbook/AccountBookCalendar.vue
+++ src/components/accountbook/AccountBookCalendar.vue
@@ -7,6 +7,7 @@
       disable-page-swipe
       is-expanded
       v-model="searchMonth"
+      @update:from-page="fnOnClickMonth"
     >
       <template v-slot:day-content="{ day, attributes }">
         <div class="flex flex-col h-full z-10 overflow-hidden">
@@ -37,11 +38,12 @@ export default {
   components: {
     AccountBookCalendarAttribute,
   },
-  created() {
-    this.clickSearchDate();
-  },
   methods: {
-    async clickSearchDate() {
+    fnOnClickMonth({ year, month }) {
+      this.searchMonth = new Date(year, month - 1, 1);
+      this.searchAccountBookList();
+    },
+    async searchAccountBookList() {
       const searchStartDate = parseFormatDateString(
         getFirstDate(new Date(this.searchMonth)),
         'yyyy-MM-dd',
@@ -78,80 +80,7 @@ export default {
       masks: {
         weekdays: 'WWW',
       },
-      attributes: [
-        {
-          key: 1,
-          customData: {
-            title: 'Lunch with mom.',
-            class: 'calendar__title__outgoing calendar__title__text',
-          },
-          dates: new Date(year, month, 1),
-        },
-        {
-          key: 2,
-          customData: {
-            title: 'Take Noah to basketball practice',
-            class: 'calendar__title__outgoing calendar__title__text',
-          },
-          dates: new Date(year, month, 2),
-        },
-        {
-          key: 3,
-          customData: {
-            title: "Noah's basketball game.",
-            class: 'bg-blue-500 calendar__title__text',
-          },
-          dates: new Date(year, month, 5),
-        },
-        {
-          key: 4,
-          customData: {
-            title: 'Take car to the shop',
-            class: 'bg-indigo-500 calendar__title__text',
-          },
-          dates: new Date(year, month, 5),
-        },
-        {
-          key: 4,
-          customData: {
-            title: 'Meeting with new client.',
-            class: 'bg-teal-500 calendar__title__text',
-          },
-          dates: new Date(year, month, 7),
-        },
-        {
-          key: 5,
-          customData: {
-            title: "Mia's gymnastics practice.",
-            class: 'bg-pink-500 calendar__title__text',
-          },
-          dates: new Date(year, month, 11),
-        },
-        {
-          key: 6,
-          customData: {
-            title: 'Cookout with friends.',
-            class: 'bg-orange-500 calendar__title__text',
-          },
-          dates: { months: 5, ordinalWeekdays: { 2: 1 } },
-        },
-        {
-          key: 7,
-          customData: {
-            title: "Mia's gymnastics recital.",
-            class: 'bg-pink-500 calendar__title__text',
-          },
-          dates: new Date(year, month, 22),
-        },
-        {
-          key: 8,
-          customData: {
-            title: 'Visit great grandma.',
-            class: 'bg-red-600 calendar__title__text',
-          },
-          dates: new Date(year, month, 25),
-        },
-      ],
+      attributes: [],
     };
   },
 };
