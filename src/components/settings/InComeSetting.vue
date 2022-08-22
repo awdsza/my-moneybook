0ea9f58fbb01521c347cf6a89f8b01f775cf2ee2
awdsza <37355034+awdsza@users.noreply.github.com>
2022-08-23 01:04:20 +0900

    feat:수입/지출 컴포넌트 분리에서 동적라우팅으로 하나의 컴포넌트로 통합

--- src/components/settings/InComeSetting.vue
+++
@@ -1,7 +0,0 @@
-<template><div>수입카테고리설정</div></template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
---
+++ src/views/settings/inOutCategorySettingView.vue
@@ -0,0 +1,14 @@
+<template>
+  <inOutCategorySetting />
+</template>
+
+<script>
+import inOutCategorySetting from '@/components/settings/inOutCategorySetting.vue';
+export default {
+  components: {
+    inOutCategorySetting,
+  },
+};
+</script>
+
+<style></style>
